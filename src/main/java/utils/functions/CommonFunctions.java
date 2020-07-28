f88e34224c639e3c57bb730da70455b9e39abea3
corina.hebristean <hebricorina@gmail.com>
2020-07-28 13:49:37 +0300

    created file path for xls; created method createExcelFile()

--- src/main/java/utils/functions/CommonFunctions.java
+++ src/main/java/utils/functions/CommonFunctions.java
@@ -1,20 +1,28 @@
 package utils.functions;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.pages.PageElements;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
-public class CommonFunctions extends PageElements {
+public class CommonFunctions {
 
     public static WebDriver driver;
     private static final String Olx_Link = "https://www.olx.ro/";
+    public static final String FilePath = System.getProperty("user.dir") + File.separator + "TestData.xls";
 
 
     public static void openLink() {
@@ -56,10 +64,7 @@ public class CommonFunctions extends PageElements {
                 js.executeScript("window.scrollBy(0,1000)");
             }
         }*/
-    
-        }
-
-
+    }
 
     public static void filterByCounty(String countyName) {
         By county = By.xpath("//ul[@class='column part25']/li/a/span[text()='" + countyName + "']");
@@ -69,6 +74,21 @@ public class CommonFunctions extends PageElements {
     public static void filterByCity(String cityName) {
         By city = By.xpath("//a[@data-name='" + cityName + "']");
         generalClickPageElement(city);
+    }
+
+    public static void createExcelFile() throws IOException {
+        Workbook wb = new HSSFWorkbook();
+        FileOutputStream fileOut = new FileOutputStream(FilePath);
+
+        Sheet sheet1 = wb.createSheet("First Sheet");
+
+        Row row1 = sheet1.createRow((short) 0);
+        row1.createCell(0).setCellValue("SM");
+        row1.createCell(1).setCellValue("PRICE");
+        row1.createCell(2).setCellValue("AVERAGE");
+
+        wb.write(fileOut);
+        fileOut.close();
     }
 
 }
